23aa420c349ab18b90859f8ba2783352dc6e60a6
Raghu Betina <rbetina@gmail.com>
2013-08-19 13:56:34 -0500

    Preparing to write logic in the seeds file to read API data and save it into our database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Write some logic in here to pull data from the Facebook API and store it in our database.
+
+# For each user
+  # Pull the user's home feed
+  # For each post in the home feed,
+    # If the post contains a video,
+      # Create a new row in the posts table and populate it with API data
+      # Associate the post with the correct user
+      # Save the row
+      # If the post has comments,
+        # For each comment,
+          # Create a new row in the comments table and populate it with API data
+          # Associate the comment with the correct post
+          # Save the row
