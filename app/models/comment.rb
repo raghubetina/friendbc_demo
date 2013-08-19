4b00727d0e208b4d8ac2471d1f5472bd7dc6f048
Raghu Betina <rbetina@gmail.com>
2013-08-19 13:58:13 -0500

    Added sensible validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ActiveRecord::Base
+  validates :comment_facebook_id, uniqueness: true
+  validates :post, presence: true
+
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :post_facebook_id, uniqueness: true
+  validates :link, presence: true
+  validates :user, presence: true
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
