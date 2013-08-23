b42d695fc4adccc82a5eb3f6b91765369b00c81c
Raghu Betina <rbetina@gmail.com>
2013-08-22 19:23:22 -0500

    Parsing result

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -9,5 +9,6 @@ class AuthController < ApplicationController
     access_token_endpoint = "https://graph.facebook.com/oauth/access_token?client_id=#{app_id}&redirect_uri=#{redirect_uri}&client_secret=#{app_secret}&code=#{params[:code]}"
 
     result = open(access_token_endpoint).read
+    parsed_result = Rack::Utils.parse_query(result)
   end
 end
